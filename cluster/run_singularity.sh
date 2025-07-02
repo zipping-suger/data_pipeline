561c5e93d78decc857414fbababfa2fbca7419f5
zippingsugar <zippingsugar@gmail.com>
2025-07-02 23:12:59 +0200

    minor

--- cluster/run_singularity.sh
+++ cluster/run_singularity.sh
@@ -21,9 +21,33 @@ singularity exec \
   --env NVIDIA_DRIVER_CAPABILITIES=all \
   --env ACCEPT_EULA=Y \
   "${CONTAINER_IMAGE}" \
-  /bin/bash -c "
-  /usr/bin/python3 -u /data_pipeline/ompl_gen.py cubby free-space full-pipeline /cluster/home/yixili/raw_data/single_cubby/free &&
-  /usr/bin/python3 -u /data_pipeline/ompl_gen.py cubby mixed full-pipeline /cluster/home/yixili/raw_data/single_cubby/mixed &&
-  /usr/bin/python3 -u /data_pipeline/ompl_gen.py cubby task-oriented full-pipeline /cluster/home/yixili/raw_data/single_cubby/task
-"
-echo "Completed run."
+  /usr/bin/python3 -u /data_pipeline/ompl_gen.py cubby free-space full-pipeline /raw_data/single_cubby/free/
+echo "Completed free-space."
+
+singularity exec \
+  --nv \
+  --containall --writable-tmpfs \
+  --bind "${PIPELINE_DIR}:/data_pipeline" \
+  --bind "${RAW_DIR}:/raw_data" \
+  --env PYTHONUNBUFFERED=1 \
+  --env PYTHONPATH="/data_pipeline:\${PYTHONPATH:-}" \
+  --env NVIDIA_DRIVER_CAPABILITIES=all \
+  --env ACCEPT_EULA=Y \
+  "${CONTAINER_IMAGE}" \
+  /usr/bin/python3 -u /data_pipeline/ompl_gen.py cubby mixed full-pipeline /raw_data/single_cubby/mixed/
+echo "Completed mixed."
+
+singularity exec \
+  --nv \
+  --containall --writable-tmpfs \
+  --bind "${PIPELINE_DIR}:/data_pipeline" \
+  --bind "${RAW_DIR}:/raw_data" \
+  --env PYTHONUNBUFFERED=1 \
+  --env PYTHONPATH="/data_pipeline:\${PYTHONPATH:-}" \
+  --env NVIDIA_DRIVER_CAPABILITIES=all \
+  --env ACCEPT_EULA=Y \
+  "${CONTAINER_IMAGE}" \
+  /usr/bin/python3 -u /data_pipeline/ompl_gen.py cubby task-oriented full-pipeline /raw_data/single_cubby/task/
+echo "Completed task-oriented."
+
+echo "All runs completed."
